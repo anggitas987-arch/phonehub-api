1796b429449a516800fab5cc814a7558cc96fd85
Anggita Sari <anggitas987@gmail.com>
2026-06-13 17:28:16 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,27 @@
-
 const express = require("express");
 const cors = require("cors");
 const fs = require("fs");
-const PORT = process.env.PORT || 3000;
-const app = express();
-app.use(cors({
-    origin: [
-        "http://127.0.0.1:5500",
-        "https://listingphone-gitaphone.netlify.app"
-    ]
-}));
-app.use(express.json());
 
-app.get("/api/phones", (req,res)=>{
-  fs.readFile("./phones.json","utf8",(err,data)=>{
-    if(err) return res.status(500).json({message:"Gagal membaca data"});
-    res.json(JSON.parse(data));
-  });
+const app = express();
+
+app.use(cors({
+    origin: "*"
+}));
+
+app.get("/api/phones", (req, res) => {
+    fs.readFile("./phones.json", "utf8", (err, data) => {
+        if (err) {
+            return res.status(500).json({
+                message: "Gagal membaca data"
+            });
+        }
+
+        res.json(JSON.parse(data));
+    });
 });
 
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log(`Server berjalan di port ${PORT}`);
+    console.log("Server berjalan");
 });

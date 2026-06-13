5734190af8fa10172f198869a2dd172cecedb50c
Anggita Sari <anggitas987@gmail.com>
2026-06-13 17:37:06 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,26 +2,19 @@ const express = require("express");
 const cors = require("cors");
 const fs = require("fs");
 
-const app = express();
-
-app.use(cors({
-    origin: "*"
-}));
-
-app.get("/api/phones", (req, res) => {
-    fs.readFile("./phones.json", "utf8", (err, data) => {
-        if (err) {
-            return res.status(500).json({
-                message: "Gagal membaca data"
-            });
-        }
-
-        res.json(JSON.parse(data));
-    });
-});
-
 const PORT = process.env.PORT || 3000;
 
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/phones", (req,res)=>{
+  fs.readFile("./phones.json","utf8",(err,data)=>{
+    if(err) return res.status(500).json({message:"Gagal membaca data"});
+    res.json(JSON.parse(data));
+  });
+});
+
 app.listen(PORT, () => {
-    console.log("Server berjalan");
+    console.log(`Server berjalan di port ${PORT}`);
 });
